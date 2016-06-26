4bd898deb5f102bc842cfbdae947e2660ad2bea0
Arya Emini <arya@aryaemini.com>
2016-06-26 16:41:02 +0300

    fixes webpack hmr config

--- webpack.config.js
+++ webpack.config.js
@@ -13,29 +13,18 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: debug ? [] : [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
   ],
   resolve: {
-    moduleDirectories: ['node_modules', 'src'],
-    extensions: ['', 'js', 'jsx']
+    extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'react-hot'
-    }, {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react'],
-        plugins: ['typecheck']
-      }
+      loader: 'babel-loader'
     }]
   }
 };
